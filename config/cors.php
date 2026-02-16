242734bec68fc9c14efebf3ecb520d74460c8898
Edilson Mutisse <98753172+Mutisse@users.noreply.github.com>
2026-02-16 14:34:11 +0200

    Adiciona frontend Netlify ao CORS

--- config/cors.php
+++ config/cors.php
@@ -8,11 +8,16 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:9000', 'http://localhost:9001', 'http://localhost:8080'],
+    'allowed_origins' => [
+        'http://localhost:9000',
+        'http://localhost:9001',
+        'http://localhost:8080',
+        'https://mozpesquisa.netlify.app'  // ✅ Adicionado teu frontend
+    ],
 
     'allowed_origins_patterns' => [],
 
